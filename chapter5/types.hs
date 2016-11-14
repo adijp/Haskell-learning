d3cac9decdfaf6b1f93f5ca3fb15c3b4ac4b9f52
Aditya Jayaprakash <adityajayaprakash@gmail.com>
2016-11-14 18:52:47 -0500

    Started chapter5

---
+++ chapter5/types.hs
@@ -0,0 +1,16 @@
+-- Type signatures of different functions:
+
+-- not
+not :: Bool -> Bool
+
+-- length 
+lenght :: [a] -> Int
+
+-- concat 
+concat :: [[a]] -> [a]
+
+-- head 
+head :: [a] -> a
+
+-- (<) 
+(<) :: Ord a => a -> a -> Bool
